4d54c6b3edbaa408946f69743f5d4d939226cc63
Asgarrrr <caruellej@hotmail.fr>
2022-11-04 10:34:53 +0100

    Export class

---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+import Renderer from "./renderer";
+import AssetManager from "./assetmanager";
+import GameLoop from "./gameloop";
+import Keyboard from "./keyboard";
+
+export { Renderer, AssetManager, GameLoop, Keyboard };
