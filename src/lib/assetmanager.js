11f3aafd39341da6d482c4044dffb5983f0de4fa
Asgarrrr <caruellej@hotmail.fr>
2022-11-05 03:29:02 +0100

    Formatting and documentation

--- src/lib/assetmanager.js
+++ src/lib/assetmanager.js
@@ -1,26 +1,45 @@
-class AssetManager {
-    images = new Map();
 
-    loadImage(key, src) {
-        return new Promise((resolve, reject) => {
+/**
+ * @class       AssetManager
+ * @description AssetManager is a class that manages assets such as images, sounds, and data.
+ */
+class AssetManager {
+
+    images = new Map( );
+
+    /**
+     * @method      loadImage
+     * @description Loads an image into the AssetManager.
+     * @param       {string} key The name of the image.
+     * @param       {string} src The source of the image.
+     * @returns     {Promise}
+     */
+    loadImage( key, src ) {
+        return new Promise( ( resolve, reject ) => {
+
             const image = new Image();
-            image.onload = () => {
-                this.images.set(key, image);
-                console.log(this.images);
-                resolve();
-            };
-            image.onerror = () => reject();
+            image.onload    = () => resolve( this.images.set( key, image ) );
+            image.onerror   = () => reject();
             image.src = src;
+
         });
     }
 
-    getSpriteImage(imageKey, dx, dy) {
+    /**
+     * @method      getSpriteImage
+     * @description Returns a sprite image from the AssetManager.
+     * @param       {string} key The name of the image.
+     * @param       {number} sx The x position of the sprite.
+     * @param       {number} sy The y position of the sprite.
+     * @returns     {Image}
+     */
+    getSpriteImage( imageKey, dx, dy) {
         return {
-            image: this.images.get(imageKey),
+            image   : this.images.get( imageKey ),
             dx,
             dy,
-            width: this.images.get(imageKey).width,
-            height: this.images.get(imageKey).height
+            width   : this.images.get(imageKey).width,
+            height  : this.images.get(imageKey).height
         }
     }
 }
