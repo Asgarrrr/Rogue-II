53b95edf873cb4190823d0261de52ebf3a17c625
Asgarrrr <caruellej@hotmail.fr>
2022-11-05 03:31:37 +0100

    Formatting and documentation

--- src/class/player.js
+++ src/class/player.js
@@ -5,42 +5,50 @@ import {
 
 import Entity from "./entity";
 
+/**
+ * @class Player
+ * @extends Entity
+ * @description The player class
+ */
 class Player extends Entity {
+
     constructor() {
-        super(0, 9, 16, 0);
-        this.direction = null;
-        this.type = "player";
+        super( 0, 9, 16, 0 );
+        this.direction  = null;
+        this.type       = "player";
     }
 
+    /**
+     * @method update
+     * @description Update the position of the player ( and the camera )
+     * @returns {void}
+     */
     update() {
-        if (this.turn) {
-            let direction = null;
-            if (Keyboard.pressed("ArrowRight")) {
-                direction = "right";
-            } else if (Keyboard.pressed("ArrowLeft")) {
-                direction = "left";
-            }
-            if (Keyboard.pressed("ArrowDown")) {
-                direction = "down";
-            } else if (Keyboard.pressed("ArrowUp")) {
-                direction = "up";
-            }
 
-            if (Keyboard.pressed(" ")) {
-                this.turnDone();
-                return;
-            }
+        if ( this.turn ) {
 
-            if (direction) {
-                if (this.move(direction)) {
+            this.direction = null;
+
+            if ( Keyboard.pressed( "ArrowRight" ) )
+                this.direction = "right";
+            else if ( Keyboard.pressed( "ArrowLeft" ) )
+                this.direction = "left";
+            else if ( Keyboard.pressed( "ArrowUp" ) )
+                this.direction = "up";
+            else if ( Keyboard.pressed( "ArrowDown" ) )
+                this.direction = "down";
+
+            if ( this.direction )
+                if ( this.move( this.direction ) )
                     this.turnDone();
-                }
-            }
+
         }
+
         super.update();
 
         Renderer.camera.x = this.x;
         Renderer.camera.y = this.y;
+
     }
 }
 
