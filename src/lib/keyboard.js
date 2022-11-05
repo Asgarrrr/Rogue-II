67be0e538e817eab52a8cc71289c76c82ff43973
Asgarrrr <caruellej@hotmail.fr>
2022-11-05 03:29:38 +0100

    Formatting and documentation

--- src/lib/keyboard.js
+++ src/lib/keyboard.js
@@ -1,27 +1,60 @@
+
+/**
+ * @class Keyboard
+ * @description Keyboard class for handling keyboard input
+ */
 class Keyboard {
-  keys = {};
 
-  constructor() {
-    this.keyDown = this.keyDown.bind(this);
-    this.keyUp = this.keyUp.bind(this);
-  }
+    constructor() {
 
-  init() {
-    window.addEventListener("keydown", this.keyDown);
-    window.addEventListener("keyup", this.keyUp);
-  }
+        this.keys       = {};
+        this.keyDown    = this.keyDown.bind( this );
+        this.keyUp      = this.keyUp.bind( this );
 
-  keyDown(event) {
-    this.keys[event.key] = true;
-  }
+    }
 
-  keyUp(event) {
-    this.keys[event.key] = false;
-  }
+    /**
+     * @description Bind the keydown and keyup events
+     * @returns     { void }
+     * @memberof    Keyboard
+     */
+    init( ) {
 
-  pressed(key) {
-    return !!this.keys[key];
-  }
+        window.addEventListener( "keydown", this.keyDown );
+        window.addEventListener( "keyup", this.keyUp );
+
+    }
+
+    /**
+     * @param       { KeyboardEvent } event
+     * @returns     { void }
+     * @memberof    Keyboard
+     * @description When a key is pressed, set the key to true
+     */
+    keyDown( event ) {
+        this.keys[ event.key ] = true;
+    }
+
+    /**
+     * @param       { KeyboardEvent } event
+     * @returns     { void }
+     * @memberof    Keyboard
+     * @description When a key is released, set the key to false
+     */
+    keyUp( event ) {
+        this.keys[ event.key ] = false;
+    }
+
+    /**
+     * @param       { string } key
+     * @returns     { boolean }
+     * @memberof    Keyboard
+     * @description Check if a key is pressed
+     */
+    pressed( key ) {
+        return !!this.keys[ key ];
+    }
+
 }
 
-export default new Keyboard();
+export default new Keyboard( );
