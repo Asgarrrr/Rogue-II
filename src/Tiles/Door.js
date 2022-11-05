700d74aa86438b54be8520d8d693b53f812eda26
Asgarrrr <caruellej@hotmail.fr>
2022-11-05 03:30:17 +0100

    Creation + Method for opening a door ( but not closing ==" )

---
+++ src/Tiles/Door.js
@@ -0,0 +1,49 @@
+
+import {
+    Renderer,
+    AssetManager
+} from "../lib/index";
+
+import Tile from "../class/tile";
+
+export default class Door extends Tile {
+
+    constructor( sx, sy, x, y, blocking = false, subTiles = false ) {
+
+        super( sx, sy, x, y, blocking, subTiles );
+
+        this.frames = AssetManager.getSpriteImage( "sprites", this.sx, this.sy ).width / 16;
+        this.lastFrame  = 0;
+        this.lastUpdate = 0;
+
+    }
+
+    render() {
+
+        Renderer.render(
+            AssetManager.getSpriteImage("sprites", this.sx, this.sy ),
+            this.x,
+            this.y
+        );
+
+    }
+
+    open() {
+
+        if ( this.sx === 6 && this.sy === 3 )
+            ( this.sx = 7, this.sy = 4 );
+
+        else if ( this.sx === 6 && this.sy === 4 )
+            ( this.sx = 6, this.sy = 3, this.y = this.y - 8 );
+
+        this.blocking = false;
+
+    }
+
+    update(delta) {
+
+    }
+
+
+
+}
