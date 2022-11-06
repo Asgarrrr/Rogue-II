b2161d90ff80afc5590d71798c7e2a83c27f6c22
Asgarrrr <caruellej@hotmail.fr>
2022-11-06 18:55:53 +0100

    Added a rendering modifier, to manage 3 display "modes", "not rendered", "visible", "rendered but not updated".

--- src/class/tile.js
+++ src/class/tile.js
@@ -12,14 +12,25 @@ class Tile {
         this.y = y;
         this.blocking = blocking;
         this.subTiles = subTiles
+        this.alreadyRendered = false;
     }
 
-    render() {
+    render( state = 0 ) {
+
+        if ( !state && !this.alreadyRendered )
+            return;
+
+        if ( state == 1 && !this.alreadyRendered )
+            this.alreadyRendered = true;
+
         Renderer.render(
-            AssetManager.getSpriteImage("sprites", this.sx, this.sy),
+            AssetManager.getSpriteImage( "sprites", this.sx, this.sy ),
             this.x,
-            this.y
+            this.y,
+            this.direction,
+            state == 1 ? 2 : 1
         );
+
     }
 
     update(delta) {}
