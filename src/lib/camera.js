924e350f1feab6086b1dbe24f18f33a1ff10e569
Asgarrrr <caruellej@hotmail.fr>
2022-11-06 18:56:45 +0100

    Removal of unnecessary properties

--- src/lib/camera.js
+++ src/lib/camera.js
@@ -1,15 +1,10 @@
 class Camera {
-  cx ;
-  cy ;
-  x  = 0;
-  y  = 0;
-  // width: number = 0;
-  // height: number = 0;
 
-  constructor(width, height) {
-    this.cx = parseInt(width / 2, 10);
-    this.cy = parseInt(height / 2, 10);
-  }
+    constructor( width, height ) {
+        this.cx = parseInt( width / 2, 10 );
+        this.cy = parseInt( height / 2, 10 );
+    }
+
 }
 
 export default Camera;
