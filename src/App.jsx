e6d97c8805e8bc2467d26627445ec51b26613c31
Asgarrrr <caruellej@hotmail.fr>
2022-11-21 01:53:37 +0100

    Websocket elements removed for now

--- src/App.jsx
+++ src/App.jsx
@@ -7,51 +7,53 @@ import Game from "./class/game";
 import "./App.css"
 import io from 'socket.io-client';
 
-const socket = io();
-socket.connect( 'http://localhost:5172' );
+// const socket = io();
+// socket.connect( 'http://localhost:5172' );
 
 function App() {
 
-    const [isConnected, setIsConnected] = useState(socket.connected);
-    const [lastPong, setLastPong] = useState(null);
+    // const [isConnected, setIsConnected] = useState(socket.connected);
+    // const [lastPong, setLastPong] = useState(null);
 
     useEffect(() => {
-
-        Game.init( );
-
-        socket.on('connect', () => {
-            console.log('connected');
-            setIsConnected(true);
-        });
-
-        socket.on('disconnect', () => {
-            console.log('disconnected');
-            setIsConnected(false);
-        });
-
-        socket.on("connect_error", (err) => {
-            console.log(`connect_error due to ${err.message}`);
-        });
-
-        socket.on('pong', () => {
-            setLastPong(Date.now());
-        });
-
-        socket.on('message', (message) => {
-            console.log(message);
-        });
-
-        return () => {
-            socket.off('connect');
-            socket.off('disconnect');
-            socket.off('pong');
-            socket.off('message');
-        }
+        Game.init();
     }, []);
 
-    const sendPing = () => {
-        socket.emit('ping');
-    }
+    //     Game.init( );
+
+    //     socket.on('connect', () => {
+    //         console.log('connected');
+    //         setIsConnected(true);
+    //     });
+
+    //     socket.on('disconnect', () => {
+    //         console.log('disconnected');
+    //         setIsConnected(false);
+    //     });
+
+    //     socket.on("connect_error", (err) => {
+    //         console.log(`connect_error due to ${err.message}`);
+    //     });
+
+    //     socket.on('pong', () => {
+    //         setLastPong(Date.now());
+    //     });
+
+    //     socket.on('message', (message) => {
+    //         console.log(message);
+    //     });
+
+    //     return () => {
+    //         socket.off('connect');
+    //         socket.off('disconnect');
+    //         socket.off('pong');
+    //         socket.off('message');
+    //     }
+    // }, []);
+
+    // const sendPing = () => {
+    //     socket.emit('ping');
+    // }
 
     return (
         <>
