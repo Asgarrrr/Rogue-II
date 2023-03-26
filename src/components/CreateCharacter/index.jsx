fb38846379baecb1b747923cedaed79adb32ad93
Asgarrrr <caruellej@hotmail.fr>
2023-03-27 00:01:12 +0200

    images

--- src/components/CreateCharacter/index.jsx
+++ src/components/CreateCharacter/index.jsx
@@ -5,30 +5,27 @@ export default function CreateCharacter({}) {
     const [ _class, setClass ] = useState( 0 );
     const classNames = [ "pugilist", "priest", "warrior" ];
 
-    // pugilist > priest > warrior
-
     useEffect( () => {
 
-        const prev_class = document.getElementById( "prev_class" );
-        const next_class = document.getElementById( "next_class" );
+        console.log( "class: ", _class );
 
-        const setPrevClass = () => {
-            setClass( _class === 0 ? classNames.length - 1 : _class - 1 );
+        const prev_class = document.getElementById( "prev_class" )
+            , next_class = document.getElementById( "next_class" );
+
+        const prev_class_click = () => {
+            setClass( _class === 0 ? 0 : _class - 1 );
         }
 
-        const setNextClass = () => {
-            setClass( _class === classNames.length - 1 ? 0 : _class + 1 );
+        const next_class_click = () => {
+            setClass( _class === classNames.length - 1 ? classNames.length - 1 : _class + 1 );
         }
 
-        prev_class.addEventListener( "click", () => setPrevClass() );
-
-        next_class.addEventListener( "click", () => setNextClass() );
-
-        console.log( classNames[ _class ])
+        prev_class.addEventListener( "click", prev_class_click );
+        next_class.addEventListener( "click", next_class_click );
 
         return () => {
-            prev_class.removeEventListener( "click", () => setPrevClass() );
-            next_class.removeEventListener( "click", () => setNextClass() );
+            prev_class.removeEventListener( "click", prev_class_click );
+            next_class.removeEventListener( "click", next_class_click );
         }
 
     }, [ _class ])
@@ -37,24 +34,22 @@ export default function CreateCharacter({}) {
     return (
         <div className="CreateCharacter">
             <div className="w-full flex justify-center items-center">
-                {/* Left arrow */}
-                <div id="prev_class">&lt;</div>
-                {/* Character */}
+                <div id="prev_class" className="text-5xl">&lt;</div>
                 <div className="character">
                     <div className="character__class">
-                        <div className="character__class__name">
+                        <div className="character__class__name text-1xl">
                             <div className="character__class__name__text">
-                                { _class }
+                                { classNames[ _class ] }
                             </div>
                         </div>
                         <div className="character__class__image">
                             <div className="character__class__image__image">
-                                <img src={ `./test/${ _class }.gif` } alt={ _class } />
+                                <img src={ `./test/${ classNames[ _class ] }.gif` } alt={ _class } />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div id="next_class">&gt;</div>
+                <div id="next_class" className="text-5xl">&gt;</div>
             </div>
         </div>
     );
